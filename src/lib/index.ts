8e68241f5874f9ad991306184b69e0557769733d
Trindade Jose <trindade.jose77@gmail.com>
2026-08-25 12:47:58 +0100

    feat(lib): export extracted components from lib entrypoint

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,9 @@
-// place files you want to import through the `$lib` alias in this folder.
+export { default as TypingEngine } from './components/TypingEngine.svelte';
+export { default as ResultSummary } from './components/ResultSummary.svelte';
+export { default as HistoryTable } from './components/HistoryTable.svelte';
+export { default as SettingsForm } from './components/SettingsForm.svelte';
+export { default as TimelineChart } from './components/TimelineChart.svelte';
+
+export type { CompletedTestResult } from './components/TypingEngine.svelte';
+export type { HistoryRunItem } from './components/HistoryTable.svelte';
+export type { SettingsValues, SettingsFormProps } from './components/SettingsForm.svelte';
